b5b0c22c01f2ec083567b49b2f40a48fd162a2c7
Vikas Rathore <vikasrathore@cedcoss.com>
2022-03-22 18:48:31 +0530

    phalcon blog completed

--- src/app/controllers/LoginController.php
+++ src/app/controllers/LoginController.php
@@ -4,29 +4,34 @@ use Phalcon\Mvc\Controller;
 
 class LoginController extends Controller
 {
+
+
     public function indexAction()
     {
-    }
+        if ($this->request->isPost()) {
 
-    public function loginAction()
-    {
-        // echo "hello";
-        $email = $this->request->getPost('email');
-        $password = $this->request->getPost('password');
+            $email = $this->request->getPost('email');
+            $password = $this->request->getPost('password');
+            $checkemail = Users::findByEmail($email);
+            $user = Users::query()
+                ->where("email = '$email'")
+                ->andWhere("password = '$password'")
+                ->execute();
+            if (count($checkemail) == 0) {
+                $this->view->msg = 'User does not exist . Please Sign Up';
+            }
 
-        $user = Users::query()
-            ->where("email = '$email'")
-            ->andWhere("password = '$password'")
-            ->execute();
-
-        print_r(count($user));
-        if (count($user) > 0) {
-            if ($user[0]->username == 'admin') {
-                
-                header('Location: http://localhost:8080/admin');
-            } else {
-                $_SESSION['user'] = $user[0]->user_id;
-                header("Location: http://localhost:8080/user/index?user_id=" . $user[0]->user_id);
+            if (count($user) > 0) {
+                if ($user[0]->status == 'Aprroved') {
+                    if ($user[0]->username == 'admin') {
+                        header('Location: http://localhost:8080/admin');
+                    } else {
+                        $_SESSION['user'] = $user[0]->user_id;
+                        header("Location: http://localhost:8080/user/index?user_id=" . $user[0]->user_id);
+                    }
+                } else {
+                    $this->view->msg = 'Wait for Approval';
+                }
             }
         }
     }
--- src/app/controllers/SignupController.php
+++ src/app/controllers/SignupController.php
@@ -12,23 +12,28 @@ class SignupController extends Controller
     public function registerAction()
     {
         $user = new Users();
-
+        $checkemail = Users::findByEmail($this->request->getPost('email'));
+        if (count($checkemail) > 0) {
+            $this->view->message = "User Exists! Please use Another email address";
+        }
         $user->assign(
             $this->request->getPost(),
             [
-                'name',
-                'email'
+                'username',
+                'email',
+                'password',
             ]
         );
+        if (count($checkemail) == 0) {
+            $success = $user->save();
 
-        $success = $user->save();
+            $this->view->success = $success;
 
-        $this->view->success = $success;
-
-        if ($success) {
-            $this->view->message = "Register succesfully";
-        } else {
-            $this->view->message = "Not Register succesfully due to following reason: <br>" . implode("<br>", $user->getMessages());
+            if ($success) {
+                $this->view->message = "Register succesfully ! Wait For Account Approval";
+            } else {
+                $this->view->message = "Not Register succesfully due to following reason: <br>" . implode("<br>", $user->getMessages());
+            }
         }
     }
 }
